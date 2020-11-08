553314a42223e3a7b7eca994221b2061d8396f79
Davy de Haas <davy.dehaas98@gmail.com>
2020-11-08 16:22:18 +0100

    Add problem 1

---
+++ C#/Problem1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            for (var i = 1; i < 1000; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            
+            Console.WriteLine("The sum of all numbers below 1000 dividable by 3 and 5 is: " + sum);
+        }
+    }
+}
