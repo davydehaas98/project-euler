f888197a418c9209957347876bb213a527491df5
Davy de Haas <davy.dehaas98@gmail.com>
2020-11-22 20:30:32 +0100

    Solve problem 3

--- C#/Problem3/Program.cs
+++ C#/Problem3/Program.cs
@@ -2,12 +2,20 @@
 
 namespace Problem3
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             var number = 600851475143;
+            var prime = number;
 
+            for (var i = 1; i < prime; i++)
+            {
+                if (prime % i == 0)
+                    prime /= i;
+            }
+
+            Console.WriteLine("The largest prime factor of the number " + number + " is: " + prime);
         }
     }
 }
