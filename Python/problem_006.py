59b322326a6c217ffd518b1f9b14c615d30cbfff
Davy de Haas <davy.dehaas98@gmail.com>
2020-11-24 20:38:29 +0100

    Add problem 6 in Python

---
+++ Python/problem_006.py
@@ -0,0 +1,20 @@
+from timeit import default_timer as timer
+
+
+def problem_006():
+    square, number = 0, 0
+
+    for i in range(0, 101):
+        square += i
+        number += i * i
+
+    difference = square * square - number
+
+    return difference
+
+
+if __name__ == '__main__':
+    start = timer()
+    answer = problem_006()
+    elapsed_time = (timer() - start) * 1000
+    print("Found {} in {} ms".format(answer, elapsed_time))
