4861858ab061513694101ba8706b121ff1aea2d4
Davy de Haas <davy.dehaas98@gmail.com>
2020-11-29 21:50:06 +0100

    Solve problem 8 and 9

---
+++ Python/problem_009.py
@@ -0,0 +1,20 @@
+from timeit import default_timer as timer
+
+
+def problem_009():
+    target = 1000
+
+    for a in range(int(target / 3)):
+        for b in range(int(target / 2)):
+            c = target - a - b
+
+            if a < b < c:
+                if a ** 2 + b ** 2 == c ** 2:
+                    return a * b * c
+
+
+if __name__ == '__main__':
+    start = timer()
+    answer = problem_009()
+    elapsed_time = (timer() - start) * 1000
+    print("Found {} in {} ms".format(answer, elapsed_time))
