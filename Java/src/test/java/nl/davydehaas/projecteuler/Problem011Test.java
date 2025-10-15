3f25669bac0d82bb965bc2643c37f9a457dd7987
Davy de Haas <davy.dehaas98@icloud.com>
2025-10-15 18:19:18 +0200

    Update Problem011Test.java

--- Java/src/test/java/nl/davydehaas/projecteuler/Problem011Test.java
+++ Java/src/test/java/nl/davydehaas/projecteuler/Problem011Test.java
@@ -1,11 +1,13 @@
 package nl.davydehaas.projecteuler;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class Problem011Test {
 
     @Test
+    @Disabled
     void problem011() {
         Number result = Problem011.solve();
         Assertions.assertEquals(70600674, result);
